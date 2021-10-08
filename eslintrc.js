cdaf86b0822dfc86379de4a681bfa19c112a19ba
Thienkhuyet <38424288+Thienkhuyet@users.noreply.github.com>
2021-10-08 10:12:43 +0700

    add validate code

--- eslintrc.js
+++ eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         // suppress errors for missing 'import React' in files
         'react/react-in-jsx-scope': 'off',
         // allow jsx syntax in js files (for next.js project)
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     },
     settings: {
         react: { version: 'detect' },
